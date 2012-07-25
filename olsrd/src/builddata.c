007244cdbf3220afa11b4c372a18413945b869df
Dainux <Dainux@Fed.inux>
2012-07-25 15:34:57 +0200

    initial commit

--- olsrd/src/builddata.c
+++ olsrd/src/builddata.c
@@ -1,4 +1,4 @@
 #include "defs.h"
 const char olsrd_version[] = "olsr.org -  0.6.3-git_-hash_57344b08f367c596cfefd258c4195482  -";
-const char build_date[] = "2012-07-25 14:27:46";
+const char build_date[] = "2012-07-25 14:38:01";
 const char build_host[] = "Fed.inux";
